0fa13804f218e8c2d82dce2e04c186e340b4b690
cami <cbernardez@mail.utdt.edu>
2023-05-26 18:45:47 -0300

    cambia denominador zb por zg

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <filesystem>
 #include <map>
 
+//ordenamos los archivos en orden small, medium, large, xl
 std::map<char, int> orden = {{'s',1} , {'m',2}, {'l',3}, {'x', 4}};
 bool compareFilenames(const std::filesystem::directory_entry& a, const std::filesystem::directory_entry& b){
     return a.path().filename().string() < b.path().filename().string();
@@ -18,41 +19,46 @@ bool compareFilenames2(const std::filesystem::directory_entry& a, const std::fil
 }
 
 int main(int argc, char** argv) {
+    //ejercicio 1: implementamos la estrategia FCFS con el archivo small_1.csv
     std::string filename = "input/small_1.csv";
-    // std::cout << filename << std::endl;
-
-    TaxiAssignmentInstance instance(filename); //crea una instancia de taxis y pasajeros
+    TaxiAssignmentInstance instance(filename); //crea una instancia de taxis y pasajeros (a usar en el ejercicio 1 y 3)
     
+
     //GREEDY FCFS
     GreedySolver solver(instance); //creamos la instancia del GreedySolver
     solver.solve(); //minimiza la suma de las distancias entre taxis y pasajeros de forma 'greedy': FCFS
     TaxiAssignmentSolution solution(instance.n); //crea una instancia de solucion al problema, con los taxis y pasajeros definidos anteriormente
-    solution = solver.getSolution(); //devuelve la asginacion obtenido
-    double time_greedy = solver.getSolutionTime(); //devuelve el tiempo de ejecución del greedy solver
+    solution = solver.getSolution(); //guardamos la solucion obtenida con el GreedySolver en la instancia de TaxiAssignmentSolution creada
+    double time_greedy = solver.getSolutionTime(); //guardamos el tiempo de ejecución del GreedySolver
 
-    // //MIN COST FLOW
+
+    //ejercicio 3: implementamos la estrategia de batching propuesta en el ejercicio 2 
+    //MIN COST FLOW
     BatchingSolver mincostflow(instance); //creamos la instancia del BatchingSolver
     mincostflow.solve(); //minimiza la suma de las distancias con un grafo, modelado como flujo de costo minimo
     TaxiAssignmentSolution solution_mincostflow(instance.n);
-    solution_mincostflow = mincostflow.getSolution(); //devuelve la asginacion obtenido
-    double time_mcf = mincostflow.getSolutionTime(); //devuelve el tiempo de ejecución del min cost flow
+    solution_mincostflow = mincostflow.getSolution(); //solucion obtenida (asignaciones de taxis-pasajeros)
+    double time_mcf = mincostflow.getSolutionTime(); //tiempo de ejecucion del batching
 
 
+
+    //ejercicio 4: experimentamos sobre todos los archivos
     std::string folderPath = "input/";
     std::vector<std::filesystem::directory_entry> entries;
 
-    std::size_t numFiles = std::count_if(std::filesystem::directory_iterator(folderPath),
-    std::filesystem::directory_iterator(),[](const auto& entry) {                                        return entry.is_regular_file() && entry.path().extension() == ".csv"; });
+    // BORRAR - creo que no hace falta
+    // std::size_t numFiles = std::count_if(std::filesystem::directory_iterator(folderPath),   std::filesystem::directory_iterator(),[](const auto& entry) {                                      return entry.is_regular_file() && entry.path().extension() == ".csv"; });
 
-    entries.reserve(numFiles);
+    // entries.reserve(numFiles);
 
     int i=0;
     for(const auto& entry : std::filesystem::directory_iterator(folderPath)){
         if (entry.is_regular_file() && entry.path().extension() == ".csv"){
-            entries.push_back(entry);
+            entries.push_back(entry); //creamos un vector 'entry' con todos los archivos a usar
         }
     }
 
+    //ordenamos los archivos
     std::sort(entries.begin(), entries.end(), compareFilenames);
     std::sort(entries.begin(), entries.end(), compareFilenames2);
 
@@ -70,7 +76,7 @@ int main(int argc, char** argv) {
         }
 
         else{
-            TaxiAssignmentInstance instance(filePath); 
+            TaxiAssignmentInstance instance(filePath); //creamos la instancia a partir de cada archivo que tomamos
 
             //calculamos la funcion objetivo, tiempo de ejecucion, y distancia maxima recorrida por un taxi para buscar a un pasajero bajo esta asignacion, con el método FCFS (greedy)
             GreedySolver solver_greedy(instance); 
@@ -88,7 +94,7 @@ int main(int argc, char** argv) {
                 }
             }
 
-            //calculamos la funcion objetivo, tiempo de ejecucion, y distancia maxima recorrida por un taxi para buscar a un pasajero bajo esta asignacion, con el método Min Cost Flow
+            //calculamos la funcion objetivo, tiempo de ejecucion, y distancia maxima recorrida por un taxi para buscar a un pasajero bajo esta asignacion, con el método Min Cost Flow (batching)
             BatchingSolver solver_mcf(instance); 
             solver_mcf.solve(); 
             TaxiAssignmentSolution solution_mcf(instance.n); 
@@ -105,16 +111,26 @@ int main(int argc, char** argv) {
             }
             
             //calculamos la mejora relativa en funcion del valor de la funcion objetivo, el tiempo para calcularla, y la distancia máxima que debe recorrer un taxi de la asignacion para recoger a su pasajero correspondiente
-            double z_funcion_obj = (funcion_objetivo_greedy - funcion_objetivo_mcf)/funcion_objetivo_mcf;
-            double z_tiempo = (tiempo_greedy - tiempo_mcf)/tiempo_mcf;
-            double z_dist_max = (distancia_max_greedy - distancia_max_mcf)/distancia_max_mcf;
+            double z_funcion_obj = (funcion_objetivo_greedy - funcion_objetivo_mcf)/funcion_objetivo_greedy;
+            double z_tiempo = (tiempo_greedy - tiempo_mcf)/tiempo_greedy;
+            double z_dist_max = (distancia_max_greedy - distancia_max_mcf)/distancia_max_greedy;
 
+
+            //cargamos los datos calculados a nuestro csv
             myfile << filePath << "," << funcion_objetivo_greedy << "," << tiempo_greedy << "," << distancia_max_greedy << "," << funcion_objetivo_mcf << "," << tiempo_mcf << "," << distancia_max_mcf << "," << z_funcion_obj << "," << z_tiempo << "," << z_dist_max <<  "\n";
         }
     } 
+   
+
+    //ejercicio 7: implementamos la estrategia propuesta en el ejercicio 6, basada en el ejercicio 3, adaptada para considerar la relación entre la distancia recorrida hasta al pasajero j, y luego el viaje a realizar
+    BatchingSolver mcf(instance); 
+    mcf.solve_alternativa(); 
+    TaxiAssignmentSolution solution_mcf(instance.n);
+    solution_mcf = mcf.getSolution();
+    std::cout << "asignacion: " << std::endl << solution_mcf << std::endl;
+    std::cout << "funcion objetivo: " << mcf.getObjectiveValue() << std::endl;
+
     
-
-
     //chequeos
     // std::cout << std::endl;
 
