8e75260d5bd5768975168b07f145297520aa24b8
cami <cbernardez@mail.utdt.edu>
2023-06-02 18:29:13 -0300

    funciones de sorting

---
+++ auxiliares.cpp
@@ -0,0 +1,12 @@
+#include "auxiliares.h"
+
+//funciones que ordenan los archivos en orden small, medium, large, xl (el orden dentro de cada tamaño de instancia no es relevante)
+std::map<char, int> orden = {{'s',1} , {'m',2}, {'l',3}, {'x', 4}};
+bool compareFilenames(const std::filesystem::directory_entry& a, const std::filesystem::directory_entry& b){
+    return a.path().filename().string() < b.path().filename().string(); 
+    
+}
+
+bool compareFilenames2(const std::filesystem::directory_entry& a, const std::filesystem::directory_entry& b){
+    return orden[a.path().filename().string()[0]] <  orden[b.path().filename().string()[0]];
+}
---
+++ auxiliares.h
@@ -0,0 +1,8 @@
+#include <filesystem>
+#include <map>
+#include <vector>
+#include <string>
+
+bool compareFilenames(const std::filesystem::directory_entry& a, const std::filesystem::directory_entry& b);
+
+bool compareFilenames2(const std::filesystem::directory_entry& a, const std::filesystem::directory_entry& b);
